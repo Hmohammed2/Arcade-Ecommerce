fb3a04ace521ce1948d11bead23b71d7444a5cb5
Hamza <hamza_mohammed15@hotmail.com>
2026-01-08 22:24:55 +0000

    Added in sitemap for SEO crawlability

--- frontend/my-app/src/app/sitemaps/sitemap.ts
+++
@@ -1,6 +0,0 @@
-export default function sitemap() {
-  return [
-    { url: "https://arcadesticklabs.co.uk/sitemaps/products.xml" },
-    { url: "https://arcadesticklabs.co.uk/sitemaps/articles.xml" },
-  ];
-}
